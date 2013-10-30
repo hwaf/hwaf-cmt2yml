b0f2712bfb86fa9d6f2718b61e9ce953eb255477
Sebastien Binet <binet@cern.ch>
2013-10-30 10:35:20 +0100

    profiles: remove old 'patterns' field

--- profiles.go
+++ profiles.go
@@ -7,7 +7,6 @@ import (
 type cnvfct_t func(wscript *hlib.Wscript_t, stmt Stmt) error
 
 type Profile struct {
-	patterns map[string]string
 	features map[string][]string
 	cnvs     map[string]cnvfct_t
 }
@@ -20,69 +19,45 @@ var (
 func init() {
 	g_profiles = make(map[string]*Profile)
 	g_profiles["tdaq"] = &Profile{
-		patterns: map[string]string{
-			// TDAQCExternal
-			"declare_lcg_mapping":  "tdaq_declare_lcg_mapping",
-			"external.RPM.package": "tdaq_external_rpm_package",
-			"external.RPM.post":    "tdaq_external_rpm_post",
-			"external.RPM.preun":   "tdaq_external_rpm_preun",
-			"make_external_slinks": "tdaq_make_external_slinks",
-
-			// TDAQCPolicy/TDAQCPolicyInt
-			"check_target":          "tdaq_check_target",
-			"copy_file":             "tdaq_copy_file",
-			"global_install_dirs":   "tdaq_global_install_dirs",
-			"global_rpms":           "tdaq_global_rpms",
-			"global_rpms_macros":    "tdaq_global_rpms_macros",
-			"include_path_1":        "tdaq_include_path_1",
-			"inst_docs_auto":        "tdaq_inst_docs_auto",
-			"inst_headers_auto":     "tdaq_inst_headers_auto",
-			"inst_headers_bin_auto": "tdaq_inst_headers_bin_auto",
-			"inst_idl_auto":         "tdaq_inst_idl_auto",
-			"inst_scripts_auto":     "tdaq_inst_scripts_auto",
-			"install_apps":          "tdaq_install_apps",
-			"install_data":          "tdaq_install_data",
-			"install_dir":           "tdaq_install_dir",
-			"install_docs":          "tdaq_install_docs",
-			"install_examples":      "tdaq_install_examples",
-			"install_headers":       "tdaq_install_headers",
-			"install_libs":          "tdaq_install_libs",
-			"install_scripts":       "tdaq_install_scripts",
-			"release_inst_path":     "tdaq_release_inst_path",
-			"set_cmtpath":           "tdaq_set_cmtpath",
-			"set_release_package":   "tdaq_set_release_package",
-		},
 		features: map[string][]string{
 			"application": []string{"tdaq_application"},
 			"library":     []string{"tdaq_library"},
 		},
+		cnvs: map[string]cnvfct_t{
+			// TDAQCExternal
+			"declare_lcg_mapping":  cnv_tdaq_declare_lcg_mapping,
+			"external.RPM.package": cnv_tdaq_external_rpm_package,
+			"external.RPM.post":    cnv_tdaq_external_rpm_post,
+			"external.RPM.preun":   cnv_tdaq_external_rpm_preun,
+			"make_external_slinks": cnv_tdaq_make_external_slinks,
+
+			// TDAQCPolicy/TDAQCPolicyInt
+			"check_target":          cnv_tdaq_check_target,
+			"copy_file":             cnv_tdaq_copy_file,
+			"global_install_dirs":   cnv_tdaq_global_install_dirs,
+			"global_rpms":           cnv_tdaq_global_rpms,
+			"global_rpms_macros":    cnv_tdaq_global_rpms_macros,
+			"include_path_1":        cnv_tdaq_include_path_1,
+			"inst_docs_auto":        cnv_tdaq_inst_docs_auto,
+			"inst_headers_auto":     cnv_tdaq_inst_headers_auto,
+			"inst_headers_bin_auto": cnv_tdaq_inst_headers_bin_auto,
+			"inst_idl_auto":         cnv_tdaq_inst_idl_auto,
+			"inst_scripts_auto":     cnv_tdaq_inst_scripts_auto,
+			"install_apps":          cnv_tdaq_install_apps,
+			"install_data":          cnv_tdaq_install_data,
+			"install_dir":           cnv_tdaq_install_dir,
+			"install_docs":          cnv_tdaq_install_docs,
+			"install_examples":      cnv_tdaq_install_examples,
+			"install_headers":       cnv_tdaq_install_headers,
+			"install_libs":          cnv_tdaq_install_libs,
+			"install_scripts":       cnv_tdaq_install_scripts,
+			"release_inst_path":     cnv_tdaq_release_inst_path,
+			"set_cmtpath":           cnv_tdaq_set_cmtpath,
+			"set_release_package":   cnv_tdaq_set_release_package,
+		},
 	}
 
 	g_profiles["atlasoff"] = &Profile{
-		patterns: map[string]string{
-			// DetCommonPolicy
-			"detcommon_shared_library":       "detcommon_shared_library",
-			"detcommon_shared_named_library": "detcommon_shared_library",
-			"detcommon_header_installer":     "detcommon_install_headers",
-
-			// AtlasPolicy
-			"installed_library":       "atlas_library",
-			"named_installed_library": "atlas_library",
-			"component_library":       "atlas_component",
-			"named_component_library": "atlas_component",
-			"dual_use_library":        "atlas_dual_use_library",
-			"named_dual_use_library":  "atlas_dual_use_library",
-			"tpcnv_library":           "atlas_tpcnv_library",
-			"named_tpcnv_library":     "atlas_tpcnv_library",
-			"declare_joboptions":      "atlas_install_joboptions",
-			"declare_python_modules":  "atlas_install_python_modules",
-			"declare_scripts":         "atlas_install_scripts",
-			"declare_xmls":            "atlas_install_xmls",
-			"declare_java":            "atlas_install_java",
-
-			// AtlasReflex
-			"lcgdict": "atlas_dictionary",
-		},
 		features: map[string][]string{
 			"application": []string{"atlas_application"},
 			"library":     []string{"atlas_library"},
